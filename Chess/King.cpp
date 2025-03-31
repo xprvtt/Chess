ac70c68eb75f3d7d907ea12cdd7387296509181e
xprvtt <142792355+xprvtt@users.noreply.github.com>
2025-03-31 15:56:48 +0500

    Update King.cpp

--- Chess/King.cpp
+++ Chess/King.cpp
@@ -7,8 +7,6 @@ string King::Set_ID_FIGURE()
 
 vector<vector<bool>> King::GetMoveForFigure(int XPositionCurrent, int YPositionCurrent, const vector<vector<pair<int, string>>>& VectorLocationFigure)
 {
-	/// ИГРОК 1 НИЖНИЙ БЕЛЫЙ 
-
 	int Row = VectorLocationFigure.size();
 	int Col = VectorLocationFigure[0].size();
 
